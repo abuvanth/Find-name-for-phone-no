b69f2aa980a65d4006e40afa97902f8cca0b1455
syed abuthahir <abuvanth@gmail.com>
2017-10-21 12:08:33 +0530

    add color for text

--- ph.py
+++ ph.py
@@ -4,6 +4,8 @@ ph=raw_input("Enter the phone No ")
 r=requests.post('http://site21.way2sms.com/LocateMobile/'+ph)
 s=bs(r.content,'lxml')
 st=s.find('div',attrs={"id":"info"})
-for i in st.find_all('p'):
-    print i.text
+data=st.find_all('p')
+for details in data[:len(data)-1]:
+    print ('\033[1;32;40m              '+details.text+'\n')
+   
           
